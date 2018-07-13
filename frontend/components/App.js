9235bb27948ba366a9005aa997a6bba7e1f31515
pdelancy <pdelancy94@gmail.com>
2018-07-13 09:43:57 -0700

    modify body

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
             <div>
                 <h1>Welcome to Paul's app!</h1>
                 <p>
-                    This is the App component.
+                    This is to demo how to use git!
                 </p>
             </div>
         );
